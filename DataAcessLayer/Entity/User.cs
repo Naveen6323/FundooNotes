3c700cfe1bb9f3e017089fe00b8c45f916c69da1
navee <navee@NAVEEN>
2025-03-05 13:29:16 +0530

    Add project files.

---
+++ DataAcessLayer/Entity/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer.Entity
+{
+    public class User:IUser
+    {
+        [Key]
+        public int ID { get; set; }
+        [Column("FirstName",TypeName = "varchar(100)")]
+        public string FirstName { get; set; }
+        [Column("LastName", TypeName = "varchar(100)")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        [Column("Email", TypeName = "varchar(100)")]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(8)]
+        [Column("Password", TypeName = "varchar(100)")]
+        public string Password { get; set; }
+    }
+}
---
+++ DataAcessLayer/Migrations/20250305075451_UserMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcessLayer.Migrations
+{
+    public partial class UserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    LastName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
